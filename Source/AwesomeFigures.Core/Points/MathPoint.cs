b0b4491720603a125523583f0baac5166b0b5503
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-09-22 01:59:51 +0300

    feat: MathPoint made record

--- Source/AwesomeFigures.Core/Points/MathPoint.cs
+++ Source/AwesomeFigures.Core/Points/MathPoint.cs
@@ -2,22 +2,9 @@ using AwesomeFigures.Abstractions.Points;
 
 namespace AwesomeFigures.Core.Points;
 
-public readonly struct MathPoint : IPoint
+// A little bit of syntax sugar
+public readonly record struct MathPoint(double X, double Y) : IPoint
 {
-    public MathPoint(double x, double y)
-    {
-        X = x;
-        Y = y;
-    }
-
-    public double X { get; init; }
-    public double Y { get; init; }
-
-    public double DistanceTo(IPoint point) => 
+    public double DistanceTo(IPoint point) =>
         Math.Sqrt(Math.Pow(X - point.X, 2) + Math.Pow(Y - point.Y, 2));
-
-    public override bool Equals(object? obj) => obj is MathPoint other && Equals(other);
-    public override int GetHashCode() => HashCode.Combine(X, Y);
-
-    private bool Equals(MathPoint other) => X.Equals(other.X) && Y.Equals(other.Y);
 }
